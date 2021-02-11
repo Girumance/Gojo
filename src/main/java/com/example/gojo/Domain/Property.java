f7e70b067ff5e07e877bf007ca91603ab9b98845
root <root@localhost.localdomain>
2021-02-10 19:38:14 -0500

    something happend

--- src/main/java/com/example/gojo/Domain/Property.java
+++ src/main/java/com/example/gojo/Domain/Property.java
@@ -34,5 +34,5 @@ public class Property {
     private String location;
     private String city;
     private String OwnerId;
-    private boolean approved=true;
+    private boolean approved=false;
 }
